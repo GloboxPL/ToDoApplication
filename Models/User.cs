a00da7caecec5f2f0fea56577ba8e36fe75d8936
dawidkruczek <dawid.kruczek.1997@gmail.com>
2021-05-05 15:01:12 +0200

    Add authentication and autherization

--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,26 @@
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 namespace ToDo.Models
 {
     public class User
     {
         public int Id { get; protected set; }
+
+        [Required]  
+        [Display(Name = "Name")]
         public string Name { get; set; }
+
+        [Required]  
+        [Display(Name = "Surname")]
         public string Surname { get; set; }
+
+        [Required]  
+        [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]  
+        [DataType(DataType.Password)]  
+        [Display(Name = "Password")]
         public string Password { get; set; }
         public virtual IList<Assignment> ToDos { get; set; } = new List<Assignment>();
 
